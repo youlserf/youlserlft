25f5820ec16f4320351af179b91a47a5fe992744
youlserf <75793798+youlserf@users.noreply.github.com>
2020-12-10 11:55:54 -0500

    Add files via upload

---
+++ JS/codigo.js
@@ -0,0 +1,14 @@
+//desarrollar el codigo de los ejercicios propuestos mediante
+//funciones
+
+//desarrollo01
+function mostrar(){
+    alert("demostración de como llamar a un a función")
+}
+
+
+
+
+
+
+//desarrollo
